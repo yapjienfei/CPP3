4de2033f3082231be09ef95ad1a0ca5b362d59fd
Yap Jien Fei <yapjienfei1998@gmail.com>
2026-05-31 10:05:42 +0800

    fix :Add virtual to destructor of base class

--- ex02/ClapTrap.hpp
+++ ex02/ClapTrap.hpp
@@ -17,7 +17,7 @@ class ClapTrap
         ClapTrap(const std::string& name);
         ClapTrap(const ClapTrap& other);
         ClapTrap& operator=(const ClapTrap& other);
-        ~ClapTrap();
+        virtual ~ClapTrap();
 
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
